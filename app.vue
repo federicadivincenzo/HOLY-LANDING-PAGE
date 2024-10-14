523eb1972ed1afc9425e7c940bcd1eeee5a3dc8c
Federica Di Vincenzo <federicadivincenzo@MBP-di-Federica.lan>
2024-10-14 21:23:15 +0200

    seo

--- app.vue
+++ app.vue
@@ -8,4 +8,11 @@
 <script setup>
 import HolyFooter from "~/components/HolyFooter.vue";
 import HolyHeader from "~/components/HolyHeader.vue";
+
+useHead({
+  title: 'Holy - Habmburger & Cicchetti',
+  meta: [
+    { name: 'description', content: 'Holy Restaurant - Habmburger & Cicchetti' }
+  ]
+})
 </script>
