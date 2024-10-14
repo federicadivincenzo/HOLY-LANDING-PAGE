3617d50bba9b4bc76514d0347350ccc13f86d00a
Federica Di Vincenzo <federicadivincenzo@MBP-di-Federica.lan>
2024-10-14 18:16:37 +0200

    added font

--- pages/menu.vue
+++ pages/menu.vue
@@ -6,7 +6,7 @@ import { menuItems } from '~/constants/menu';
 <template>
   <div class="holy-menu">
     <div class="holy-menu__container">
-        <section v-for="section in menuItems">
+        <section v-for="section in menuItems" class="holy-menu__section">
             <p v-show="section.section !== 'Altro'"class="holy-menu__title">{{ section.section }}</p>
             <article v-for="item in section.items">
                 <p class="holy-menu__item-title">{{ item.title }} <span>€ {{ item.price }}</span></p>
