a3bd9c0eae7ceedae6902862a9a64a3725c97dca
Federica Di Vincenzo <federicadivincenzo@MBP-di-Federica.lan>
2024-10-14 17:58:23 +0200

    menu page

---
+++ pages/menu.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { menuItems } from '~/constants/menu';
+
+</script>
+
+<template>
+  <div class="holy-menu">
+    <div class="holy-menu__container">
+        <section v-for="section in menuItems">
+            <p v-show="section.section !== 'Altro'"class="holy-menu__title">{{ section.section }}</p>
+            <article v-for="item in section.items">
+                <p class="holy-menu__item-title">{{ item.title }} <span>€ {{ item.price }}</span></p>
+                <p>{{ item.description }}</p>
+            </article>
+        </section>
+
+    </div>
+   </div>
+</template>
